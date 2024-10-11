0c0b9cdcdd13b0f36c127271527eb6bd931d330b
Rika <melty.dupre@gmail.com>
2024-10-10 22:22:10 -0400

    Minor edited on main.py.

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "POS app"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, item_name: Union[str, None] = None):
+    return {"item_id": item_id, "item_name": item_name}
